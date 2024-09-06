30279918adc2eecce7c6915972a9b93e9452be8e
hmontes <hugomontes927@gmail.com>
2024-09-06 16:32:03 -0400

    Sesion 09 - Captura de pantalla - ScreenShot Username "Already exists"

--- src/test/java/screenshot/Screen.java
+++ src/test/java/screenshot/Screen.java
@@ -1,8 +1,6 @@
 package screenshot;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -11,7 +9,14 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Screen {
 
@@ -19,7 +24,7 @@ public class Screen {
     WebDriverWait wait;
 
     @BeforeClass
-    public void setup(){
+    public void setup() {
         //CONFIGURAR EL WEBDRIVER
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver.exe");
         driver = new ChromeDriver();
@@ -74,6 +79,21 @@ public class Screen {
         Username.sendKeys("admin");
         Thread.sleep(3000);
 
+        // CAPTURA DE PANTALLA CUANDO SE TIENE UN USUARIO REGISTRADO
+        // VERIFICAMOS QUE EXISTA EL MENSAJE "Already exists"
+        try {
+            WebElement alreadyExistsElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[contains(.,'Already exists')]")));
+            // VERIFICAR SI SE MUESTRA EL MENSAJE
+            if (alreadyExistsElement.isDisplayed()) {
+                System.out.println("El texto 'Already exists' esta visible.");
+                takeScreenShot("Already exists");
+            }
+
+        } catch (TimeoutException ex) {
+            // SI NO APARECE EL MENSAJE
+            System.out.println("El texto 'Already exists' no es visible");
+        }
+
         WebElement Password = driver.findElement(By.xpath("(//input[@type='password'])[1]"));
         Password.sendKeys("IQUATTRo.2024");
         Thread.sleep(3000);
@@ -83,6 +103,26 @@ public class Screen {
         WebElement btnSave = driver.findElement(By.xpath("//button[contains(.,'Save')]"));
         btnSave.click();
         Thread.sleep(3000);
+    }
+
+    // METODO PARA SACAR CAPTURA DE PANTALLA
+    private void takeScreenShot(String fileName) {
+        try {
+            File screenShotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            // GENERAR NOMBRE PARA UNA CAPTURA UNICA
+            String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+            String screenShotName = fileName + "_" + timestamp + ".png";
+            // DEFINIR LA RUTA DONDE SE GUARDARA EL ARCHIVO O CAPTURA DE PANTALLA
+            Path destination = Paths.get("screenshots", screenShotName);
+            // CREAR EL DIRECTORIO SI NO EXISTE
+            Files.createDirectories(destination.getParent());
+            // COPIAMOS EN EL DESTINO
+            Files.copy(screenShotFile.toPath(), destination);
+            System.out.println("ScreenShot save " + destination.toAbsolutePath());
+        } catch (IOException ex) {
+            // MANEJAMOS CUALQUIER ERROR QUE OCURRE
+            System.err.println("Error al sacar la captura de pantalla " + ex.getMessage());
+        }
     }
 
     @AfterClass
